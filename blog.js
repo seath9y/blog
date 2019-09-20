46641789f4e090697887e803ccf0dff1a9b0012c
Seath9y <seath9y@outlook.com>
2019-09-20 20:39:46 +0800

    9/20

--- blog.js
+++ blog.js
@@ -4,32 +4,21 @@ var fs = require('fs')
 var blogFilePath = 'blog.json'
 
 
-// 这是一个用来存储 Blog 数据的对象
+// 用来存储 Blog 数据的对象
 const ModelBlog = function(form) {
-    // || 是一种新的写法, 在 js 圈子里太过流行, 所以记住即可
-    // a = b || c 意思是如果 b 是 undefined 或者 null 就把 c 赋值给 a
     this.title = form.title || ''
     this.author = form.author || ''
     this.content = form.content || ''
-    // 生成一个 unix 时间, unix 时间是什么, 上课会说
+    // 生成一个 unix 时间
     this.created_time = Math.floor(new Date() / 1000)
 }
 
-const loadBlogs = function(callback) {
-    // 确保文件有内容, 这里就不用处理文件不存在或者内容错误的情况了
-    // 注意, 一般都是这样不处理的
+const loadBlogs = function() {
     var content = fs.readFileSync(blogFilePath, 'utf8')
     var blogs = JSON.parse(content)
     return blogs
 }
 
-/*
-b 这个对象是我们要导出给别的代码用的对象
-它有一个 data 属性用来存储所有的 blogs 对象
-它有 all 方法返回一个包含所有 blog 的数组
-它有 new 方法来在数据中插入一个新的 blog 并且返回
-他有 save 方法来保存更改到文件中
-*/
 var b = {
     data: loadBlogs()
 }
@@ -67,6 +56,11 @@ b.save = function() {
     })
 }
 
-// 导出一个对象的时候用 module.exports = 对象 的方式
-// 这样引用的时候就可以直接把模块当这个对象来用了(具体看使用方法)
 module.exports = b
+/*
+b 对象
+data 属性用来存储所有的 blogs 对象
+all 方法返回一个包含所有 blog 的数组
+new 方法来在数据中插入一个新的 blog 并且返回
+save 方法来保存更改到文件中
+*/
